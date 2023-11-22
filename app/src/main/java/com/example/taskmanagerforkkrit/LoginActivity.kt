f08e3472e7a1043fad82127a4a8a5eee2f39b01f
Grisha Kuznetsov <awtershokk@gmail.com>
2023-11-23 00:58:07 +0700

    бэк для входа/регистрации

--- app/src/main/java/com/example/taskmanagerforkkrit/LoginActivity.kt
+++ app/src/main/java/com/example/taskmanagerforkkrit/LoginActivity.kt
@@ -1,11 +1,90 @@
 package com.example.taskmanagerforkkrit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+
+    private lateinit var loginUsername: EditText
+    private lateinit var loginPassword: EditText
+    private lateinit var loginButton: Button
+    private lateinit var signupRedirectText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        loginUsername = findViewById(R.id.login_username)
+        loginPassword = findViewById(R.id.login_password)
+        loginButton = findViewById(R.id.login_button)
+        signupRedirectText = findViewById(R.id.signupRedirectText)
+
+        loginButton.setOnClickListener {
+            val email = loginUsername.text.toString()
+            val password = loginPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+
+                        // Вход успешен
+                        Toast.makeText(this, "Вход успешен", Toast.LENGTH_SHORT).show()
+
+                        getUserRole(user) { role ->
+                            handleUserRole(role)
+                        }
+
+                    } else {
+                        Toast.makeText(baseContext, "Ошибка входа: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        signupRedirectText.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun getUserRole(user: FirebaseUser?, onComplete: (String) -> Unit) {
+        val uid = user?.uid
+        val userRef = database.getReference("users").child(uid ?: "")
+        userRef.child("role").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val role = dataSnapshot.value.toString()
+                onComplete(role)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Обработка ошибок
+            }
+        })
+    }
+
+    private fun handleUserRole(role: String) {
+        if (role == "Руководитель") {
+            Toast.makeText(this, "Вы руководитель", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Вы сотрудник", Toast.LENGTH_SHORT).show()
+        }
+        finish()
     }
 }
--- app/src/main/java/com/example/taskmanagerforkkrit/RegisterActivity.kt
+++ app/src/main/java/com/example/taskmanagerforkkrit/RegisterActivity.kt
@@ -1,11 +1,91 @@
 package com.example.taskmanagerforkkrit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+
+    private lateinit var signupName: EditText
+    private lateinit var signupEmail: EditText
+    private lateinit var signupUsername: EditText
+    private lateinit var signupPassword: EditText
+    private lateinit var spinner: Spinner
+    private lateinit var signupButton: Button
+    private lateinit var loginRedirectText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        signupName = findViewById(R.id.signup_name)
+        signupEmail = findViewById(R.id.signup_email)
+        signupUsername = findViewById(R.id.signup_username)
+        signupPassword = findViewById(R.id.signup_password)
+        spinner = findViewById(R.id.spinner)
+        signupButton = findViewById(R.id.signup_button)
+        loginRedirectText = findViewById(R.id.loginRedirectText)
+
+        val roles = arrayOf("Руководитель", "Сотрудник")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, roles)
+        spinner.adapter = adapter
+
+        signupButton.setOnClickListener {
+            val email = signupEmail.text.toString()
+            val password = signupPassword.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        val role = spinner.selectedItem.toString()
+                        val name = signupName.text.toString()
+                        val username = signupUsername.text.toString()
+
+                        saveUserData(user, name, username, email, role)
+
+                        Toast.makeText(this, "Регистрация успешна", Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(baseContext, "Ошибка регистрации: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        loginRedirectText.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun saveUserData(user: FirebaseUser?, name: String, username: String, email: String, role: String) {
+        val uid = user?.uid
+        val userRef = database.getReference("users").child(uid ?: "")
+
+        val userData = HashMap<String, Any>()
+        userData["name"] = name
+        userData["username"] = username
+        userData["email"] = email
+        userData["role"] = role
+
+        userRef.setValue(userData)
     }
 }
